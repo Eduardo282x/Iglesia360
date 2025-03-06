ca5ab7eca693a475390f812a0d5d1a3946649668
Eduardo <eduardokunmixyeah@gmail.com>
2025-03-05 22:46:39 -0400

    config router

--- Iglesia360/src/App.tsx
+++ Iglesia360/src/App.tsx
@@ -1,9 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import { Home } from "./pages/Home/Home";
+import { User } from "./pages/User/User";
+import { Assistens } from "./pages/Assistens/Assistens";
 function App() {
 
   return (
     <>
-      <p className=" m-20">Tailwind</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/usuario" element={<User />} />
+          <Route path="/asistencia" element={<Assistens />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
