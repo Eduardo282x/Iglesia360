7d6f63f0e2be24d26543e4d07b786ae8253495a8
Eduardo282x <eduardokunmixyeah@gmail.com>
2025-03-24 16:39:46 -0400

    fix login

--- Iglesia360/src/pages/Login/Login.tsx
+++ Iglesia360/src/pages/Login/Login.tsx
@@ -1,23 +1,41 @@
+import { useForm } from 'react-hook-form';
 import logo from '../../assets/logo1.jpg';
 
+interface ILogin {
+    username: string;
+    password: string;
+}
+
 export const Login = () => {
+
+    const { register, handleSubmit } = useForm<ILogin>({
+        defaultValues: {
+            username: '',
+            password: ''
+        }
+    });
+
+    const onSubmit = (data: ILogin) => {
+        console.log(data);
+    }
+
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="h-screen overflow-hidden">
 
-            <div className="h-[55%] bg-[#0a1543] flex justify-center items-end pb-6">
-
+            <div className="h-[60%] bg-[#0a1543] flex items-center justify-center">
                 <img src={logo} alt="Logo" className="h-120" />
             </div>
 
-            <div className="h-[45%] bg-white flex justify-center items-start relative">
+            <div className="h-[30%] bg-white flex justify-center items-start relative">
 
-                <div className="absolute top-[-120px] bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
+                <div className="absolute -top-28 bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
                     <h2 className="text-center text-2xl font-bold text-gray-900 mb-6">Iniciar Sesión</h2>
 
-                    <form className="space-y-4">
+                    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
                         <div>
                             <input
                                 type="text"
+                                {...register('username')}
                                 placeholder="Usuario"
                                 className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
                             />
@@ -25,6 +43,7 @@ export const Login = () => {
                         <div>
                             <input
                                 type="password"
+                                {...register('password')}
                                 placeholder="Contraseña"
                                 className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-indigo-500"
                             />
@@ -32,7 +51,7 @@ export const Login = () => {
                         <div>
                             <button
                                 type="submit"
-                                className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700"
+                                className="w-full bg-blue-600 text-white py-2 rounded-md cursor-pointer hover:bg-blue-700"
                             >
                                 Ingresar
                             </button>
